d6c231f0b093bd878ac60269c9bb0ed2417d448c
khannasiddharth <siddharth.khanna40@yahoo.co.uk>
2019-06-05 12:39:18 +0100

    Add files via upload

---
+++ NOTES.sql
@@ -0,0 +1,8 @@
+Program - 
+C:\Users\siddharthkhanna\Desktop\UD\Big_Data_Analytics_with_Apache_Spark_and_Scala\RB-Scala\mapreduce-java\exercise.java
+
+Compilation -
+C:\Users\siddharthkhanna\Desktop\UD\Big_Data_Analytics_with_Apache_Spark_and_Scala\RB-Scala\mapreduce-java\compile.sh
+
+Execution -
+C:\Users\siddharthkhanna\Desktop\UD\Big_Data_Analytics_with_Apache_Spark_and_Scala\RB-Scala\mapreduce-java\exec.sh
